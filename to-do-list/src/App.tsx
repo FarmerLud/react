af2d99337823ccf950b2b1292557c20e30c024b0
FarmerL <fludenab@uni.pe>
2023-05-04 18:22:17 -0500

    initializing project

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,36 +1,34 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '../../../../../vite.svg'
 import './App.css'
-import Task from './components/Task/Task'
 
-function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: '1',
-      text: 'Esta es una tarea',
-      datetime: ''
-    },
-    {
-      id: '2',
-      text: 'Segunda tarea del día',
-      datetime: '2023-04-30 10:00:00'
-    },
-  ])
+function App (): JSX:Element {
+  const [count, setCount] = useState(0)
 
   return (
-    <section>
-      <div>English</div>
-      <h1 className='title'>YOU CAN DO IT !</h1>
-      <ul className='tasks-box'>
-        <h2 className='groupTitle'>TODAY</h2>
-        {
-          tasks.map((task,i) => {
-            return (
-              <Task key={i} task={task}></Task>
-            )
-          })
-        }
-      </ul>
-    </section>
+    <>
+      <div>
+        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
+        </a>
+        <a href='https://react.dev' target='_blank' rel='noreferrer'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className='read-the-docs'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
